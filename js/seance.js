89e8b9391dd03d1602cad5c7acd41e97789fde55
xriss <plated@github.actions>
2023-10-22 23:23:52 +0000

    xriss published a site update

---
+++ js/seance.js
@@ -0,0 +1,9 @@
+const $c7536631c917bf2d$var$seance = {};
+var $c7536631c917bf2d$export$2e2bcd8739ae039 = $c7536631c917bf2d$var$seance;
+$c7536631c917bf2d$var$seance.start = function(opts) {
+    console.log("SEE YANCE 3");
+};
+
+
+export {$c7536631c917bf2d$export$2e2bcd8739ae039 as default};
+//# sourceMappingURL=seance.js.map
