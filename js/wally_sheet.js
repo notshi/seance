27ee79a4fe14804bbc66dd963e968218533779b8
kriss <kriss@xixs.com>
2023-10-26 23:54:17 +0100

    need special prefix

--- js/wally_sheet.js
+++ js/wally_sheet.js
@@ -28,29 +28,31 @@ wally_sheet.start=async function(opts)
 {
 	let qcsv=await load_csv( opts.dirname+"/csv/sheets/question.csv" )
 
-	console.log(qcsv)
+//	console.log(qcsv)
 
 	let filename="/csv/jobs/question.csv"
 	console.log("working on "+filename)
 
 	let jobs=[]
 	jobs.push(["id","text"])
-	jobs.push(["prompt","{text:wrap_reword_question}"])
+	jobs.push(["predict","128"])
 
 	for(let it of qcsv )
 	{
 		jobs.push(["prompt","{text:wrap_reword_question}"])
 		jobs.push(["id",it.id+"_question"])
+		jobs.push(["prefix","{doyou}"])
 		jobs.push(["text",it.question])
 		jobs.push(["run",5])
 
-		for(let i=1;i++;i<=4)
+		for( let i=1 ; i<=4 ; i++ )
 		{
 			let answer=it["answer"+i]
 			if(answer)
 			{
 				jobs.push(["prompt","{text:wrap_reword_answer}"])
 				jobs.push(["id",it.id+"_answer"+i])
+				jobs.push(["prefix","{imy}"])
 				jobs.push(["text",answer])
 				jobs.push(["run",5])
 			}
