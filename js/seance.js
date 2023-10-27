3ab8fbaf32c243921985663dd13418d74f4f6765
kriss <kriss@xixs.com>
2023-10-27 20:16:57 +0100

    flesh out data

--- js/seance.js
+++ js/seance.js
@@ -3,10 +3,42 @@ const seance={}
 export default seance
 
 import { default as plated_module } from "plated"
+import { parse as csv_parse } from "csv-parse/sync"
 
 import text_csv from "../csv/sheets/text.csv"
-import { parse as csv_parse } from "csv-parse/sync"
-let csv=csv_parse(text_csv,{relax_column_count:true,columns:true})
+let texts=csv_parse(text_csv,{relax_column_count:true,columns:true})
+let textids={} ; for(let v of texts ) { textids[v.id]=textids[v.id]||[] ; (textids[v.id]).push(v.text) }
+
+import image_csv from "../csv/sheets/image.csv"
+let images=csv_parse(image_csv,{relax_column_count:true,columns:true})
+let imageids={} ; for(let image of images )
+{
+	image.idx=Number(image.id.substr(5))
+	imageids[image.idx]=image
+	image.emotion=image.emotion.trim().toLowerCase()
+	image.max_question=0
+	for(let i=1;i<20;i++)
+	{
+		let id=image.emotion+i+"_question"
+		if(!textids[id]){break}
+		image.max_question=i
+	}
+
+// assign questions to each image
+	let idx=image.idx*3 // start with this question (might need to wrap)
+	image.questions=[]
+	for(let i=0;i<5;i++)
+	{
+		let id=image.emotion+idx+"_question" // might not exist
+		while(!textids[id]) // wrap
+		{
+			idx=idx-image.max_question // wrap
+			id=image.emotion+idx+"_question" // try this
+		}
+		image.questions[i]=image.emotion+idx // this should be a stable list
+		idx=idx+1 // next
+	}
+}
 
 seance.start=async function(opts)
 {
@@ -14,7 +46,8 @@ seance.start=async function(opts)
 	let plated=plated_module.create({})
 		
 	console.log("SEE YANCE")
-	console.log(csv)
+	console.log(textids)
+	console.log(imageids)
 
 // load full plated json for this site
 	let map=await (await fetch("./plated.map.json") ).json()
