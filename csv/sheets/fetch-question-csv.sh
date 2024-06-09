7af399177597f7b4c9f1e014bb3cd050ad6b4f09
kriss <kriss@xixs.com>
2024-06-10 00:46:34 +0100

    .

---
+++ csv/sheets/fetch-question-csv.sh
@@ -0,0 +1,3 @@
+cd `dirname $0`
+
+wget "https://docs.google.com/spreadsheets/d/e/2PACX-1vSBHyChWlfasoYlEMMUOUOFg5pll_UFSsNRqRGNxvQijFcfN-2_nFJN8_Uo9ZRR3ktnB4Nho_gkWpUh/pub?gid=403115949&single=true&output=csv" -O question.csv
